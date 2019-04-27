dd63840bd4e567ac9dea46be09917b2621532941
shai atias <shai@experitest.com>
2019-04-27 19:56:08 +0300

    Handle cart merge on login

--- src/main/java/com/example/demo/service/MongoUserDetailsService.java
+++ src/main/java/com/example/demo/service/MongoUserDetailsService.java
@@ -1,6 +1,8 @@
 package com.example.demo.service;
 
 
+import com.example.demo.domain.Cart;
+import com.example.demo.domain.ProductInCart;
 import com.example.demo.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -12,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,6 +27,15 @@ public class MongoUserDetailsService implements UserDetailsService {
     @Autowired
     private UsersRepository usersRepository;
 
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private CartService cartService;
+
+    @Autowired
+    private UserService userService;
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
@@ -33,8 +45,46 @@ public class MongoUserDetailsService implements UserDetailsService {
             throw new UsernameNotFoundException("user not found");
         }
 
+        handleCartMerge(user);
+
         List<SimpleGrantedAuthority> roles = user.getRoles().stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
 
         return new User(user.getEmail(), user.getPassword(), roles);
+    }
+
+    private void handleCartMerge(com.example.demo.domain.User user) {
+
+        Cart cart = cartService.getCart(userService.getUserIdFromRequest(request));
+        Cart newCart = cartService.getCart(user.getEmail());
+
+        cartService.cartsRepository.save(mergeCart(cart, newCart));
+
+        // reset old cart
+        cartService.cartsRepository.delete(cart);
+
+    }
+
+    private Cart mergeCart(Cart cart, Cart newCart) {
+
+        for (ProductInCart p : cart.getProducts().values()) {
+
+            // item is new
+            if (newCart.getProducts().get(p.getProductId()) == null) {
+                newCart.getProducts().put(p.getProductId(), p);
+            }
+
+            // old items, combine quantity
+            else {
+
+                ProductInCart productInCart = cart.getProducts().get(p.getProductId());
+                ProductInCart newProductInCart = newCart.getProducts().get(p.getProductId());
+
+                newProductInCart.setQuantity(
+                        newProductInCart.getQuantity() + productInCart.getQuantity()
+                );
+            }
+        }
+
+        return newCart;
     }
 }
